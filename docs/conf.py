2e100cdf823c63dd54c1e802b9a1211fe34ae5cc
Indian Modassir <indianmodassir@gmail.com>
2025-08-14 02:27:08 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,75 +1,48 @@
-from datetime import datetime
+# conf.py for Sphinx + Read the Docs
 
-# import specific project config
-import os, sys
-sys.path.append(os.curdir)
-from conf_project import *
+import os
+import sys
+import datetime
 
-author = 'Anton Smirnov'
-copyright = '{}'.format(datetime.now().year)
-language = 'en'
+# Add current directory to sys.path
+sys.path.insert(0, os.path.abspath('.'))
 
-html_title = project
-html_theme = 'sphinx_book_theme'
-templates_path = ["_templates"]
-html_sidebars = {
-    "**": [
-        "navbar-logo.html",
-        "icon-links.html",
-        "rtd-version.html",
-        "search-button-field.html",
-        "sbt-sidebar-nav.html",
-    ]
+# -- Project information -----------------------------------------------------
+
+project = 'dotenv'
+author = 'lazervel'
+copyright = f'{datetime.datetime.now().year}, {author}'
+release = '1.0.0'  # You can sync this with your repo release
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'myst_parser',           # for Markdown support
+]
+
+# Source file types
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
 }
-html_theme_options = {
-    'use_edit_page_button': True,
-    'icon_links': [
-        {
-            "name": "GitLab",
-            "url": "https://gitlab.com/sandfox/" + repo,
-            "icon": "fa-brands fa-square-gitlab",
-            "type": "fontawesome",
-        },
-        {
-            "name": "GitHub",
-            "url": "https://github.com/arokettu/" + repo,
-            "icon": "fa-brands fa-square-github",
-            "type": "fontawesome",
-        },
-        {
-            "name": "Codeberg",
-            "url": "https://codeberg.org/sandfox/" + repo,
-            "icon": "fa-solid fa-mountain",
-            "type": "fontawesome",
-        },
-        {
-            "name": "Gitea",
-            "url": "https://sandfox.org/sandfox/" + repo,
-            "icon": "fa-solid fa-mug-hot",
-            "type": "fontawesome",
-        },
-   ]
-}
-if packagist:
-    html_theme_options['icon_links'].append({
-        "name": "Packagist",
-        "url": "https://packagist.org/packages/" + packagist,
-        "icon": "https://img.shields.io/packagist/dm/" + packagist + "?style=flat-square",
-        "type": "url",
-    })
-if npmjs:
-    html_theme_options['icon_links'].append({
-        "name": "NPM",
-        "url": "https://www.npmjs.com/package/" + npmjs,
-        "icon": "https://img.shields.io/npm/dm/" + npmjs + "?style=flat-square",
-        "type": "url",
-    })
+
+# The master toctree document.
+master_doc = 'index'
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- GitHub integration ------------------------------------------------------
+
 html_context = {
-    'current_version': os.environ.get("READTHEDOCS_VERSION_NAME"),
-    'gitlab_user': "sandfox",
-    'gitlab_repo': repo,
-    'gitlab_version': "master",
-    'doc_path': "docs",
+    'display_github': True,
+    'github_user': 'lazervel',
+    'github_repo': 'dotenv',
+    'github_version': 'main',
+    'conf_py_path': '/docs/',
 }
 
-exclude_patterns = ['venv/*']
+# -- Misc --------------------------------------------------------------------
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
