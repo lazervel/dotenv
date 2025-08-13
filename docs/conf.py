bc7376f30399e6dc525a4bb22e1c4a5eff45b107
Indian Modassir <indianmodassir@gmail.com>
2025-08-14 02:30:37 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,39 +1,38 @@
-# conf.py for Sphinx + Read the Docs
+# docs/conf.py
 
 import os
 import sys
 import datetime
 
 # Add current directory to sys.path
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
 project = 'dotenv'
 author = 'lazervel'
 copyright = f'{datetime.datetime.now().year}, {author}'
-release = '1.0.0'  # You can sync this with your repo release
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 
 extensions = [
-    'myst_parser',           # for Markdown support
+    'myst_parser',  # enables Markdown parsing
 ]
 
-# Source file types
+# Only support markdown
 source_suffix = {
-    '.rst': 'restructuredtext',
     '.md': 'markdown',
 }
 
-# The master toctree document.
-master_doc = 'index'
+# Set README.md as the main doc
+master_doc = 'README'
 
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
 
-# -- GitHub integration ------------------------------------------------------
+# -- GitHub integration (optional) -------------------------------------------
 
 html_context = {
     'display_github': True,
@@ -43,6 +42,6 @@ html_context = {
     'conf_py_path': '/docs/',
 }
 
-# -- Misc --------------------------------------------------------------------
+# -- Other configs -----------------------------------------------------------
 
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
