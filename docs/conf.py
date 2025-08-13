e7a9e5aa8ffdcfaef399bea12fe40640209fff09
Indian Modassir <indianmodassir@gmail.com>
2025-08-14 02:39:30 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,33 +4,25 @@ import os
 import sys
 import datetime
 
-# Add root path
+# Add root directory to sys.path
 sys.path.insert(0, os.path.abspath('..'))
 
-# -- Project info ------------------------------------------------------------
-
+# -- Project info --
 project = 'dotenv'
 author = 'lazervel'
 copyright = f'{datetime.datetime.now().year}, {author}'
 release = '1.0.0'
 
-# -- General configuration ---------------------------------------------------
-
+# -- General configuration --
 extensions = ['myst_parser']
-
-# Support Markdown
 source_suffix = {
     '.md': 'markdown',
 }
+master_doc = 'index'
 
-# Use README.md as the root document
-master_doc = 'README'
-
-# -- HTML output -------------------------------------------------------------
-
+# -- HTML output --
 html_theme = 'sphinx_rtd_theme'
 
-# Optional: GitHub integration
 html_context = {
     'display_github': True,
     'github_user': 'lazervel',
@@ -38,7 +30,5 @@ html_context = {
     'github_version': 'main',
     'conf_py_path': '/docs/',
 }
-
-# -- Other -------------------------------------------------------------------
 
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
